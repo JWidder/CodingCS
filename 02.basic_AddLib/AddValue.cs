25e314f795f6fcd4d3b8524606c90bd57f8ee416
Johannes Widder <ajwidder@yahoo.de>
2021-09-20 20:20:37 +0200

    Old examples added

---
+++ 02.basic_AddLib/AddValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.basic_AddLib
+{
+    class AddValue  
+    {
+        int AddValues(int in1, int in2) => in1 + in2;
+    }
+}
